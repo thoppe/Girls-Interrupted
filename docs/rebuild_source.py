91896a83536303632dbe112a0e9199330b51aa0f
Travis Hoppe <travis.hoppe@gmail.com>
2018-02-03 15:53:43 -0500

    Added source movies

---
+++ docs/rebuild_source.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import glob, os
+
+F_MOVIE = glob.glob("../raw_videos/*")
+F_MOVIE = map(os.path.basename, glob.glob("../raw_videos/*"))
+
+cols = ['name', 'year']
+
+df = pd.DataFrame()
+df["source"] = F_MOVIE
+df = df.set_index("source")
+
+dfx = pd.read_csv("source_movies.csv")
+dfx = dfx.set_index('source')
+
+for col in cols:
+    df[col] = dfx[col]
+
+df.to_csv("source_movies.csv")
+
