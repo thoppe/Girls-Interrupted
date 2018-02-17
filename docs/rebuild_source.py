ee8f26a2e39defd08c32c3513b0a4ac8a524ceee
Travis Hoppe <travis.hoppe@gmail.com>
2018-02-17 13:19:18 -0500

    Extract IMDB id

--- docs/rebuild_source.py
+++ docs/rebuild_source.py
@@ -13,8 +13,13 @@ df = df.set_index("source")
 dfx = pd.read_csv("source_movies.csv")
 dfx = dfx.set_index('source')
 
-for col in cols:
+for col in dfx.columns:
     df[col] = dfx[col]
+
+# Extract the IMDB ID from the URL from those that don't have it
+idx = pd.isnull(df["IMDB_ID"]) & (~pd.isnull(df["IMDB_url"]))
+func = lambda x:int(x.split('/')[-2].strip('t'))
+df.ix[idx, "IMDB_ID"] = df.ix[idx, "IMDB_url"].apply(func)
 
 df.to_csv("source_movies.csv")
 
