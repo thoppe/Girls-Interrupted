9118fe5733c21577fa723be7c0a2fc77d6f75ca5
Travis Hoppe <travis.hoppe@gmail.com>
2018-02-17 13:20:16 -0500

    Extract IMDB id

--- docs/rebuild_source.py
+++ docs/rebuild_source.py
@@ -4,8 +4,6 @@ import glob, os
 F_MOVIE = glob.glob("../raw_videos/*")
 F_MOVIE = map(os.path.basename, glob.glob("../raw_videos/*"))
 
-cols = ['name', 'year','IMDB_rating']
-
 df = pd.DataFrame()
 df["source"] = F_MOVIE
 df = df.set_index("source")
@@ -20,6 +18,12 @@ for col in dfx.columns:
 idx = pd.isnull(df["IMDB_ID"]) & (~pd.isnull(df["IMDB_url"]))
 func = lambda x:int(x.split('/')[-2].strip('t'))
 df.ix[idx, "IMDB_ID"] = df.ix[idx, "IMDB_url"].apply(func)
+
+# Cross-reference data from https://shannonvturner.com/bechdel
+f_shannon = "~/datasets/shannonvturnercom_bechdel_full.csv"
+dfs = pd.read_csv(f_shannon)
+print dfs
+exit()
 
 df.to_csv("source_movies.csv")
 
