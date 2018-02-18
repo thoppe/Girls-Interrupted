020ff7f732525ec5afe3db4e4402cc91cecd8ed1
Travis Hoppe <travis.hoppe@gmail.com>
2018-02-17 22:47:35 -0500

    Add missing movie

--- docs/rebuild_source.py
+++ docs/rebuild_source.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import glob, os
+import glob, os, json
 
 F_MOVIE = glob.glob("../raw_videos/*")
 F_MOVIE = map(os.path.basename, glob.glob("../raw_videos/*"))
@@ -14,14 +14,25 @@ dfx = dfx.set_index('source')
 for col in dfx.columns:
     df[col] = dfx[col]
 
+'''
 # Clean IMDB URLS
+df.ix[pd.isnull(df.IMDB_url), "IMDB_url"] = ""
 func = lambda x:x.split('?')[0]
-df["IMDB_url"] = df["IMDB_url"].apply(func)
+df["IMDB_url"] = df["IMDB_url"].astype(str).apply(func)
     
 # Extract the IMDB ID from the URL from those that don't have it
 idx = pd.isnull(df["IMDB_ID"]) & (~pd.isnull(df["IMDB_url"]))
+idx = idx & (df.IMDB_url!='')
 func = lambda x:int(x.split('/')[-2].strip('t'))
 df.ix[idx, "IMDB_ID"] = df.ix[idx, "IMDB_url"].apply(func)
+print df.IMDB_ID.values
+exit()
+'''
+
+# Fill in missing IMDB_IDs
+idx = pd.isnull(df.IMDB_ID)
+df.ix[idx, 'IMDB_ID'] = df.index[idx]
+
 
 # Cross-reference data from https://shannonvturner.com/bechdel
 f_shannon = "~/datasets/shannonvturnercom_bechdel_full.csv"
@@ -34,13 +45,26 @@ df = df.set_index("IMDB_ID")
 replace_cols = [
     ["BECHDEL_rating","bechdel_rating"],
     ["IMDB_rating","imdb_rating"],
-    #["RT_rating","tomato_meter"],
-    #["RT_USER_rating","tomato_user_meter"],
 ]
 
 for col1, col2 in replace_cols:
     idx = df.index[df[col1].isnull()]
     df.ix[idx, col1] = dfs.ix[idx, col2]
+
+
+# Gather information about the movies from the summary
+for f_json in glob.glob("../data/summary/*.json"):
+    with open(f_json) as FIN:
+        js = json.loads(FIN.read())
+    source = os.path.basename(js.pop('f_movie'))
+
+    if (df.source==source).sum() != 1:
+        print "Weirdness with movie", f_json
+        continue
+
+    continue
+    exit()
+
 
 
 df.sort_values("year").to_csv("source_movies.csv")
